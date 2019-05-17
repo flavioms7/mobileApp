034b5f2f5dc4b6b3f3d370fbc91c5239d038eb97
Flavio Miranda <flavio.m-silva@sefaz.pe.gov.br>
2019-05-17 11:47:24 -0300

    Aula API Rest seção 7, aula 28

---
+++ src/main/java/com/appsdeveloperblog/app/ws/Usuario.java
@@ -0,0 +1,33 @@
+package com.appsdeveloperblog.app.ws;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String nome;
+	private String sobrenome;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
+
+}
